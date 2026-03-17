8099a6013f3fa566676309ef362004015fb56dd9
dyunyunn <dyunita839@gmail.com>
2026-03-17 22:29:01 +0700

    Upload tugas backtracking N-Queen
    Program Python untuk menyelesaikan N-Queen dengan backtracking

---
+++ nqueen.py
@@ -0,0 +1,108 @@
+Coba posisi (0,0)
+Letakkan Queen
+Q . . .
+. . . .
+. . . .
+. . . .
+
+Coba posisi (1,0)
+Coba posisi (1,1)
+Coba posisi (1,2)
+Letakkan Queen
+Q . . .
+. . Q .
+. . . .
+. . . .
+
+Coba posisi (2,0)
+Coba posisi (2,1)
+Coba posisi (2,2)
+Coba posisi (2,3)
+Backtrack
+Q . . .
+. . . .
+. . . .
+. . . .
+
+Coba posisi (1,3)
+Letakkan Queen
+Q . . .
+. . . Q
+. . . .
+. . . .
+
+Coba posisi (2,0)
+Coba posisi (2,1)
+Letakkan Queen
+Q . . .
+. . . Q
+. Q . .
+. . . .
+
+Coba posisi (3,0)
+Coba posisi (3,1)
+Coba posisi (3,2)
+Coba posisi (3,3)
+Backtrack
+Q . . .
+. . . Q
+. . . .
+. . . .
+
+Coba posisi (2,2)
+Coba posisi (2,3)
+Backtrack
+Q . . .
+. . . .
+. . . .
+. . . .
+
+Backtrack
+. . . .
+. . . .
+. . . .
+. . . .
+
+Coba posisi (0,1)
+Letakkan Queen
+. Q . .
+. . . .
+. . . .
+. . . .
+
+Coba posisi (1,0)
+Coba posisi (1,1)
+Coba posisi (1,2)
+Coba posisi (1,3)
+Letakkan Queen
+. Q . .
+. . . Q
+. . . .
+. . . .
+
+Coba posisi (2,0)
+Letakkan Queen
+. Q . .
+. . . Q
+Q . . .
+. . . .
+
+Coba posisi (3,0)
+Coba posisi (3,1)
+Coba posisi (3,2)
+Letakkan Queen
+. Q . .
+. . . Q
+Q . . .
+. . Q .
+
+SOLUSI DITEMUKAN
+
+. Q . .
+. . . Q
+Q . . .
+. . Q .
+
+
+
+** Process exited - Return Code: 0 **
